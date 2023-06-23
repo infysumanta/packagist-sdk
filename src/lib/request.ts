1888a581ffa08b7d6ead4c0d5081f428cc03f32e
Sumanta Kabiraj <sumantablog@gmail.com>
2023-06-23 18:12:14 +0530

    added node fetch with json for rerquest

---
+++ src/lib/request.ts
@@ -0,0 +1,7 @@
+import fetch from 'node-fetch';
+
+export const getRequest = async (url: string): Promise<any> => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
